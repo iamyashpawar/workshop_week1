deb8e9a01e22bfe8585cb1f5bfc6f2bd6c768854
Yash <imyashpawar@gmail.com>
2023-04-10 20:54:40 +0530

    Added: Distinct Elements

--- src/com/bridgelabz/distinct_element.java
+++ src/com/bridgelabz/distinct_element.java
@@ -8,14 +8,14 @@ public class distinct_element
 
         int[] arr1 = {10,20,30,54,74};
         int[] arr2 = {15,34,43,20,10};
-
+       int [] arr3 = {};
         for(int i=0; i<= arr1.length;i++)
         {
             for (int j =0 ; j<= arr2.length; j++)
             {
                 if (arr1[i] != arr2[j])
                 {
-                    System.out.println(arr2[j]);
+                    System.out.println(arr1[i]);
                 }
             }
         }
